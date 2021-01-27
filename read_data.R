116bd9475858dee73d793ed35cc6b46b56af9536
Simon Wolanin <sw@awp.ch>
2021-01-27 08:34:54 +0100

    Update

---
+++ read_data.R
@@ -0,0 +1,56 @@
+library(tabulizer)
+library(dplyr)
+library(readxl)
+library(DBI)
+library(RMySQL)
+
+#Funktion Datenbankverbindung schliessen
+dbDisconnectAll <- function(){
+  ile <- length(dbListConnections(MySQL())  )
+  lapply( dbListConnections(MySQL()), function(x) dbDisconnect(x) )
+  cat(sprintf("%s connection(s) closed.\n", ile))
+}
+
+setwd("C:/Users/simon/OneDrive/R/Impfungen/Daten")
+
+
+impfdaten <- read_excel("20210124_Impfdaten_BAG.xlsx")
+impfdaten$`Gelieferte Impfdosen` <- gsub("'","",impfdaten$`Gelieferte Impfdosen`)
+impfdaten$`Gelieferte Impfdosen` <- gsub("'","",impfdaten$`Gelieferte Impfdosen`)
+impfdaten$`Gelieferte Impfdosen` <- as.numeric(gsub("'","",impfdaten$`Gelieferte Impfdosen`))
+impfdaten$`Bislang total verimpft` <- gsub("'","",impfdaten$`Bislang total verimpft`)
+impfdaten$`Bislang total verimpft` <- gsub("'","",impfdaten$`Bislang total verimpft`)
+impfdaten$`Bislang total verimpft` <- as.numeric(gsub("'","",impfdaten$`Bislang total verimpft`))
+
+letter <- c("a","a","b","c")
+
+for (i in 2:ncol(impfdaten)) {
+
+ID <- paste0("20210124","_",letter[i])
+datum <- "2021-01-24"
+Typ <- colnames(impfdaten)[i]
+
+#In Datenbank einlesen
+mydb <- dbConnect(MySQL(), user='awp', password='rs71MR3!', dbname='covid', host='32863.hostserv.eu', encoding="utf8")
+
+#Daten in Datenbank einlesen
+sql_qry <- "INSERT IGNORE INTO impfungen(ID,datum,Typ,CH,GE,VD,VS,FR,NE,JU,BE,SO,BS,BL,AG,ZH,SH,TG,AR,AI,SG,GL,SZ,ZG,LU,NW,OW,UR,GR,TI,FL) VALUES"
+sql_qry <- paste0(sql_qry, paste(sprintf("('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',
+                                         '%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',
+                                         '%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',
+                                         '%s')",
+                                         ID,datum,Typ,
+                                         impfdaten[1,i],impfdaten[2,i],impfdaten[3,i],impfdaten[4,i],impfdaten[5,i],
+                                         impfdaten[6,i],impfdaten[7,i],impfdaten[8,i],impfdaten[9,i],impfdaten[10,i],
+                                         impfdaten[11,i],impfdaten[12,i],impfdaten[13,i],impfdaten[14,i],impfdaten[15,i],
+                                         impfdaten[16,i],impfdaten[17,i],impfdaten[18,i],impfdaten[19,i],impfdaten[20,i],
+                                         impfdaten[21,i],impfdaten[22,i],impfdaten[23,i],impfdaten[24,i],impfdaten[25,i],
+                                         impfdaten[26,i],impfdaten[27,i],impfdaten[28,i]
+                                         ), collapse = ","))
+dbGetQuery(mydb, "SET NAMES 'utf8'")
+rs <- dbSendQuery(mydb, sql_qry)
+
+#Datenbankverbindungen schliessen
+dbDisconnectAll()
+
+}
---
+++ transform_data.R
@@ -0,0 +1,98 @@
+library(tabulizer)
+library(dplyr)
+library(readxl)
+library(DBI)
+library(RMySQL)
+library(tidyr)
+library(readr)
+
+#Funktion Datenbankverbindung schliessen
+dbDisconnectAll <- function(){
+  ile <- length(dbListConnections(MySQL())  )
+  lapply( dbListConnections(MySQL()), function(x) dbDisconnect(x) )
+  cat(sprintf("%s connection(s) closed.\n", ile))
+}
+
+setwd("C:/Users/simon/OneDrive/R/Impfungen")
+
+#Impfdaten abrufen
+mydb <- dbConnect(MySQL(), user='awp', password='rs71MR3!', dbname='covid', host='32863.hostserv.eu')
+rs <- dbSendQuery(mydb, "SELECT * FROM impfungen")
+impfdaten <- fetch(rs,n=-1)
+
+dbDisconnectAll()
+
+impfdaten <- impfdaten[,-c(1,4,31)]
+
+#Kantonsnamen abrufen
+kantone <- read_csv("Daten/MASTERFILE_GDE_NEW.csv")
+kantone <- unique(kantone[,c(2,9:11)])
+
+
+#Transform Data
+long <- impfdaten %>%
+  gather(area,value,-c(datum,Typ))
+
+long$datum <- as.Date(long$datum)
+last_date <- long$datum[nrow(long)]
+last_date_string <- format(last_date,"%d.%m.%Y")
+
+long_verimpft_aktuell <- long[long$Typ == "Bislang total verimpft" & long$datum == last_date,]
+long_verimpft_last_week <- long[long$Typ == "Bislang total verimpft" & long$datum == last_date-3,] #7
+long_verimpft_second_last_week <- long[long$Typ == "Bislang total verimpft" & long$datum == last_date-3,] #14
+long_verimpft_second_last_week$value <- long_verimpft_second_last_week$value - 1000
+long_impfdosen <- long[long$Typ == "Gelieferte Impfdosen" & long$datum == last_date,]
+long_verimpft_pro_person <- long[long$Typ == "Geimpfte Dosen pro 100 Einwohner" & long$datum == last_date,]
+
+#Create Data Frame
+impfdaten_dw <- data.frame("Kanton_short","Datum",999,999,
+                  999,999,999,999,999)
+
+colnames(impfdaten_dw) <- c("Kanton_Short","Datum","Verimpft","Verimpft_pro_Person",
+                            "Verimpft_pro_Tag","Verimpft_pro_Tag_Vorwoche","Veränderung","Geliefert","Verimpft_Anteil")
+
+for (i in 1:26) {
+
+kanton_short <- long_verimpft_aktuell$area[i]
+verimpft <- long_verimpft_aktuell$value[i]
+verimpft_pro_person <- long_verimpft_pro_person$value[i]
+verimpft_pro_tag <- (long_verimpft_aktuell$value[i]-long_verimpft_last_week$value[i])/7
+verimpft_pro_tag_vorwoche <- (long_verimpft_last_week$value[i]-long_verimpft_second_last_week$value[i])/7
+veraenderung <- (verimpft_pro_tag*100)/verimpft_pro_tag_vorwoche-100  
+geliefert <- long_impfdosen$value[i]
+verimpft_anteil <- (long_verimpft_aktuell$value[i]/long_impfdosen$value[i])*100
+
+new_data <- data.frame(kanton_short,last_date_string,verimpft,verimpft_pro_person,
+                       verimpft_pro_tag,verimpft_pro_tag_vorwoche,veraenderung,
+                       geliefert,verimpft_anteil)
+
+colnames(new_data) <- c("Kanton_Short","Datum","Verimpft","Verimpft_pro_Person",
+                            "Verimpft_pro_Tag","Verimpft_pro_Tag_Vorwoche","Veränderung","Geliefert","Verimpft_Anteil")
+
+impfdaten_dw <- rbind(impfdaten_dw,new_data)
+  
+}
+
+#Adapt dw Data
+impfdaten_dw <- impfdaten_dw[-1,]
+impfdaten_dw$Verimpft <- format(impfdaten_dw$Verimpft,big.mark = "'")
+impfdaten_dw$Verimpft_pro_Tag <- format(round(impfdaten_dw$Verimpft_pro_Tag,0),big.mark = "'")
+impfdaten_dw$Verimpft_pro_Tag_Vorwoche <- format(round(impfdaten_dw$Verimpft_pro_Tag_Vorwoche,0),big.mark = "'")
+impfdaten_dw$Veränderung <- round(impfdaten_dw$Veränderung,0)
+impfdaten_dw$Geliefert <- format(impfdaten_dw$Geliefert,big.mark = "'")
+impfdaten_dw$Verimpft_Anteil <- round(impfdaten_dw$Verimpft_Anteil,0)
+
+#Merge mit Kantonsnamen
+impfdaten_dw <- merge(impfdaten_dw,kantone)
+
+#Create_Text
+impfdaten_dw$Text_d <- paste0("Im Kanton ",impfdaten_dw$Kanton_d," wurden bislang por 100 Einwohner <b>",
+                              impfdaten_dw$Verimpft_pro_Person,"</b> Impfungen durchgeführt.",
+                              " Das entspricht <b>",impfdaten_dw$Verimpft,"</b> Impfungen.<br><br>",
+                              "In der vergangenen Woche wurden pro Tag durchschnittlich <b>",impfdaten_dw$Verimpft_pro_Tag,
+                              "</b> Personen geimpft. Im Vergleich zur Vorwoche eintspricht dies einer Veränderung von <b>",
+                              impfdaten_dw$Veränderung,"%</b>.<br><br>",
+                              "Insgesamt wurden in den Kanton ",impfdaten_dw$Kanton_d," bislang <b>",impfdaten_dw$Geliefert,
+                              "</b> Impfdosen geliefert. Davon wurden bereits <b>",impfdaten_dw$Verimpft_Anteil,"%</b> verimpft.<br><br>",
+                              "<i>Stand: ",impfdaten_dw$Datum,"</i>")
+                              
